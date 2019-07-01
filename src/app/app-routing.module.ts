f4980f52461a6e592ac83629f856bbe7c9a988f5
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-01 14:33:52 -0300

    Add the guard to routes that you want to protect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from "./services/auth-guard.service";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'inside', loadChildren: './pages/inside/inside.module#InsidePageModule' },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
+  {
+    path: "inside",
+    loadChildren: "./pages/inside/inside.module#InsidePageModule",
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
